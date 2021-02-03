788bb4b45f627564afb22a4225050dca919e8885
David-B-Comer <davec2233@gmail.com>
2021-02-03 18:25:42 -0500

    added order service

---
+++ src/main/java/com/davec/ECommerce/service/OrderService.java
@@ -0,0 +1,17 @@
+package com.davec.ECommerce.service;
+
+import com.davec.ECommerce.model.Order;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@Validated
+public interface OrderService {
+
+    @NotNull Iterable<Order> getAllOrders();
+
+    Order create(@NotNull(message = "The order cannot be null.") @Valid Order order);
+
+    void update(@NotNull(message = "The order cannot be null.") @Valid Order order);
+}
