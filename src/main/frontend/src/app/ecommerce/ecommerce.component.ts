3482e8e630048cce83bfb5b4522816f17b3ccbc8
David-B-Comer <davec2233@gmail.com>
2021-02-05 18:42:45 -0500

    added ecommerce component ts

--- src/main/frontend/src/app/ecommerce/ecommerce.component.ts
+++ src/main/frontend/src/app/ecommerce/ecommerce.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-
 @Component({
-  selector: 'app-ecommerce',
-  templateUrl: './ecommerce.component.html',
-  styleUrls: ['./ecommerce.component.css']
+selector: 'app-ecommerce',
+templateUrl: './ecommerce.component.html',
+styleUrls: ['./ecommerce.component.css']
 })
 export class EcommerceComponent implements OnInit {
+private collapsed = true;
+orderFinished = false;
 
-  constructor() { }
+@ViewChild('productsC')
+productsC: ProductsComponent;
 
-  ngOnInit(): void {
-  }
+@ViewChild('shoppingCartC')
+shoppingCartC: ShoppingCartComponent;
 
+@ViewChild('ordersC')
+ordersC: OrdersComponent;
+
+toggleCollapsed(): void {
+        this.collapsed = !this.collapsed;
+    }
+
+    finishOrder(orderFinished: boolean) {
+        this.orderFinished = orderFinished;
+    }
+
+    reset() {
+        this.orderFinished = false;
+        this.productsC.reset();
+        this.shoppingCartC.reset();
+        this.ordersC.paid = false;
+    }
 }
